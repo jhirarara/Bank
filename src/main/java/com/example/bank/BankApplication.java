fb7a93b10d89e59508ecf571d2af4637af2b2fdb
anna <anna@annas-MacBook-Pro.local>
2025-02-17 07:59:55 +0800

    hello test

--- src/main/java/com/example/bank/BankApplication.java
+++ src/main/java/com/example/bank/BankApplication.java
@@ -13,40 +13,11 @@ import java.util.stream.Collectors;
 public class BankApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
-        String name = "ReportSubmissionFileUpload_History";
-        List<String> k = Arrays.asList(
-                "InternalId",
-                "ReportSubmissionFileUploadId",
-                "DocEntry",
-                "ReportTypeId",
-                "FileTypeId",
-                "FileTypeAbbreviation",
-                "SubmissionDate",
-                "FileName",
-                "FilePath",
-                "FileExtension",
-                "FileSize",
-                "MimeType",
-                "ReportTypeCode",
-                "StatusId",
-                "StatusName",
-                "ActionId",
-                "UserSign",
-                "Timestamp",
-                "InstanceId"
-        );
 
 
 
-        // Add "hello" to each element
-        List<String> result = k.stream()
-                .map(s -> "create index " + s +" on "+name+" "+"("+s+");")  // Concatenate "hello" with each element
-                .collect(Collectors.toList());
 
 
-
-        // Print the result
-        result.forEach(System.out::println);
 
     }
 }
