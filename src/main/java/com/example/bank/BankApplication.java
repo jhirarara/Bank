ec83a77b52259081ef61912a8de96302e5308a7a
anna <anna@annas-MacBook-Pro.local>
2025-02-20 08:21:33 +0800

    remove comment

--- src/main/java/com/example/bank/BankApplication.java
+++ src/main/java/com/example/bank/BankApplication.java
@@ -1,20 +1,16 @@
 package com.example.bank;
-import java.io.*;
-import java.nio.file.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @SpringBootApplication
 public class BankApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
-        
+
 
 
 
